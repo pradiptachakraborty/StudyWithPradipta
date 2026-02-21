129e3054b75183cc55d227f8b093545c9bfa0148
Pradipta Chakraborty <139611742+pradiptachakraborty@users.noreply.github.com>
2026-02-21 17:38:07 +0530

    added css and js file
    style.css and script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function openCard() {
+  document.getElementById("infoCard").classList.add("active");
+}
+
+function closeCard() {
+  document.getElementById("infoCard").classList.remove("active");
+}
