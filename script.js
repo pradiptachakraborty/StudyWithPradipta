1bb99c96b28f9bd7865ffed771262c8982fb86c5
Pradipta Chakraborty <139611742+pradiptachakraborty@users.noreply.github.com>
2026-02-21 19:52:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,4 +4,9 @@ function openCard() {
 
 function closeCard() {
   document.getElementById("infoCard").classList.remove("active");
+  document.getElementById("infoCardcloudcomputing").classList.remove("active");
+}
+
+function openCardCloudComputing() {
+  document.getElementById("infoCardcloudcomputing").classList.add("active");
 }
