12cb83ebf36beceafacda034258576cac30cc60d
Pradipta Chakraborty <139611742+pradiptachakraborty@users.noreply.github.com>
2026-03-20 14:34:05 +0530

    add new but for ml and iot

--- script.js
+++ script.js
@@ -5,8 +5,18 @@ function openCard() {
 function closeCard() {
   document.getElementById("infoCard").classList.remove("active");
   document.getElementById("infoCardcloudcomputing").classList.remove("active");
+  document.getElementById("infoCardMachineLearning").classList.remove("active");
+  document.getElementById("infoCardInternetOfThings").classList.remove("active");
 }
 
 function openCardCloudComputing() {
   document.getElementById("infoCardcloudcomputing").classList.add("active");
+}
+
+function openCardMachineLearning(){
+  document.getElementById("infoCardMachineLearning").classList.add("active");
+}
+
+function openCardInternetOfThings(){
+  document.getElementById("infoCardInternetOfThings").classList.add("active");
 }
